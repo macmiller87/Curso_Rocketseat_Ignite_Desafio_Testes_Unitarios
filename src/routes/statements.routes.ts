419e4d7e9830e4bdece7c4abec5fd0db4db79c5c
Macmiller <macamagolf@gmail.com>
2022-05-03 21:45:49 -0300

    Criando rota userTransfer

--- src/routes/statements.routes.ts
+++ src/routes/statements.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateStatementController } from '../modules/statements/useCases/createStatement/CreateStatementController';
+import { CreateTransferUserController } from '../modules/statements/useCases/createTransfer/CreateTransferUserController';
 import { GetBalanceController } from '../modules/statements/useCases/getBalance/GetBalanceController';
 import { GetStatementOperationController } from '../modules/statements/useCases/getStatementOperation/GetStatementOperationController';
 import { ensureAuthenticated } from '../shared/infra/http/middlwares/ensureAuthenticated';
@@ -9,6 +10,7 @@ const statementRouter = Router();
 const getBalanceController = new GetBalanceController();
 const createStatementController = new CreateStatementController();
 const getStatementOperationController = new GetStatementOperationController();
+const createTransferUserController = new CreateTransferUserController();
 
 statementRouter.use(ensureAuthenticated);
 
@@ -16,5 +18,6 @@ statementRouter.get('/balance', getBalanceController.execute);
 statementRouter.post('/deposit', createStatementController.execute);
 statementRouter.post('/withdraw', createStatementController.execute);
 statementRouter.get('/:statement_id', getStatementOperationController.execute);
+statementRouter.post('/transfer/:recieve_userId', createTransferUserController.execute);
 
 export { statementRouter };
